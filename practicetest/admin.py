129e15805109ac3d03cfd23ac2b93d4b1dfd78cb
ravin-rakholiya <ravinkumarrakh@gmail.com>
2023-07-13 17:49:04 -0400

    registred models on django admin

--- practicetest/admin.py
+++ practicetest/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Permission
+
+from practicetest.models import *
 
 # Register your models here.
+class PracticeTestAdmin(admin.ModelAdmin):
+    fields = ['test_type','question_type','question','option','answer']
+    list_display = ('id', 'test_type','question_type','question','option','answer','created_at','updated_at')
+    list_per_page = 25
+
+admin.site.register(PracticeTest, PracticeTestAdmin)
+
+class UserPracticeAdmin(admin.ModelAdmin):
+    fields = ['user','practice_test','is_challanged','is_bookmarked']
+    list_display = ('id', 'user','practice_test','is_challanged','is_bookmarked','created_at','updated_at')
+    list_per_page = 25
+
+admin.site.register(UserPractice, UserPracticeAdmin)
